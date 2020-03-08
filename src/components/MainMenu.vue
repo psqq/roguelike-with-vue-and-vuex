b4d27c7958cbfe58371fa9fa634969a8141c5abb
psqq <pestov-sa@ya.ru>
2020-03-08 20:54:30 +0300

    Start game

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <h1>Main menu</h1>
-    <button>Start new game</button>
-    <br><br>
+    <button @click="startGame">Start new game</button>
+    <br />
+    <br />
     <button>High scores</button>
   </div>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+import * as game from "../store/modules/game";
+
 export default {
-  name: 'MainMenu',
-}
+  name: "MainMenu",
+  methods: mapMutations([game.mutations.startGame])
+};
 </script>
 
 <style scoped>
