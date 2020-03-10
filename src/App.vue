0cb5234a8562af81ec2a366905bc40440ef111a1
psqq <pestov-sa@ya.ru>
2020-03-10 22:06:17 +0300

    New nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
-    <router-link to="/">Главное меню</router-link> |
-    <router-link to="/game">Текущая игра</router-link> |
-    <router-link to="/about">Об игре</router-link>
-    <hr>
+    <nav>
+      <router-link to="/" class="link">Главное меню</router-link>
+      <router-link to="/game" class="link">Текущая игра</router-link>
+      <router-link to="/about" class="link">Об игре</router-link>
+    </nav>
+    <hr />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App"
+};
 </script>
 
 <style>
+nav {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, auto));
+  justify-items: center;
+}
+nav .link {
+  text-decoration: none;
+  color: black;
+}
 </style>
