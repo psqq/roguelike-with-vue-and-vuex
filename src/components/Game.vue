a33cc1db9598dcf65d0b0027dac088fe12a7039e
Sergey Pestov <pestov-sa@yandex.ru>
2020-03-11 21:45:28 +0300

    new zen game screen

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <h1>Игра</h1>
-    <div v-if="isGameStarted">
-      Игра запущена.
-      <h2>Текущий уровень</h2>
-      <Level />
-    </div>
-    <div v-else>Игра не запущена.</div>
+  <div class="wrapper">
+    <template v-if="isGameStarted">
+      <div class="left-bar">
+        <div>HP: 100</div>
+        <div>Gold: 0</div>
+      </div>
+      <div class="content">
+        <Level />
+      </div>
+    </template>
+    <div class="content" v-else>Игра не запущена.</div>
   </div>
 </template>
 
@@ -26,4 +29,29 @@ export default {
 </script>
 
 <style scoped>
+.wrapper {
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  grid-template-columns: 1fr repeat(4, auto) 1fr;
+  grid-template-rows: 1fr repeat(2, auto) 1fr;
+  grid-template-areas:
+    ". . . . . ."
+    ". l a a r ."
+    ". l a a r ."
+    ". . . . . .";
+}
+.content {
+  grid-area: a;
+  align-self: center;
+  justify-self: center;
+  border: 1px solid black;
+  padding: 10px;
+}
+.left-bar {
+  grid-area: l;
+  align-self: center;
+  justify-self: center;
+  padding: 10px;
+}
 </style>
